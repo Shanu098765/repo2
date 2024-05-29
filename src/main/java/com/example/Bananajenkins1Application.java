9d03565d3a69ab12d88882080bacc186c3607fb7
Shanu Kumar <Shanu.Kumar2@BRILLIO.COM>
2024-05-29 15:28:51 +0530

    first commit

---
+++ src/main/java/com/example/Bananajenkins1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bananajenkins1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bananajenkins1Application.class, args);
+	}
+
+}
